2a65244e55364e069e4e0e2d2f493d80e22fb3e2
Matteo <matteop18@gmail.com>
2020-03-17 15:27:57 +0100

    impostazione iniziale layouts generale

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,2 +1,9 @@
 {{-- Struttura base --}}
+{{-- Includo l'header e il footer che ho creato nella cartella partials --}}
+@include('layouts.partials._header')
 
+<header></header>
+<main></main>
+<footer></footer>
+
+ @include('layouts.partials._footer')
