9aa79377c0115f090a22620c77e91ef5dced27a3
Matteo <matteop18@gmail.com>
2020-03-17 18:41:38 +0100

    ruolo e azienda

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -4,7 +4,8 @@
 {{-- Posso usare config anche per ciclarci dentro --}}
     @foreach (config('students.allStudents') as $item)
         <img src="{{$item['poster']}}" alt="">
-        {{$item['name']}} ({{$item['age']}})
+        <h2>{{$item['name']}} ({{$item['age']}})</h2>
+        <p>Lavora da {{$item['company']}} come {{$item['role']}}</p>
     @endforeach
 @endsection
 
