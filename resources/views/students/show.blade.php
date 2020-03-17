bbf9c9bd8965ce6d618280c23162851d4e6ed5f2
Matteo <matteop18@gmail.com>
2020-03-17 20:06:04 +0100

    al click sull'immagine apro quella scheda

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,1 +1,19 @@
-@dd($data)
+@extends('layouts.layout')
+
+@section('main')
+<div class="container">
+    <div class="card-student">
+        <div class="student">
+                <img src="{{$data['poster']}}" alt="">
+            <div class="date-student">
+                <h3>{{$data['name']}} ({{$data['age']}})</h3>
+                <p>Lavora da {{$data['company']}} come {{$data['role']}}</p>
+            </div>
+        </div>
+        <p class="description">{{$data['description']}}</p>
+        <div class="box-icon">
+            <i class="fab fa-linkedin"></i>
+        </div>
+    </div>
+</div>
+@endsection
