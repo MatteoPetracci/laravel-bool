6a48cffb463aacb3aeedc73db3eaf4da389735a5
Matteo <matteop18@gmail.com>
2020-03-17 16:46:43 +0100

    helper config

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,16 +6,32 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    private $students;
+    
+    public function __construct()
+    {
+        $this->getStudents();
+    }
+
     public function index() 
     {
-
+        $students = $this->students;
+        return view('students.index', compact('students'));
     }
     
-    // Questa funzione deve ricevere un dato in entrata per mostrare quello che mi serve in questo l'id
+    // Questa funzione deve ricevere un dato in entrata per mostrare quello che mi serve in questo caso l'id
 
     public function show($id) 
     {
         
+    }
+        // Configurazione tramite l'helper config 
+        // Creo un file php nella cartella config
+
+    private function getStudents() {
+        // Nome del file php e nome della chiave del mio array
+        $this->students = config('students.allStudents');
+        dd($this->students);
     }
 }
 
---
+++ config/students.php
@@ -0,0 +1,13 @@
+<?php 
+// In questo file voglio ritornare un array in questo caso tutti gli studenti 
+// Creo questo file in modo tale che questo file è accessibile ovunque in Laravel
+return [
+  'allStudents' =>  [
+      'poster' => 'https://www.boolean.careers/images/students/biagini.png',
+      'name'=> 'Alessandro Biagini',
+      'age'=> 25,
+      'company'=> 'DISC SPA',
+      'role'=> 'Web Developer',
+      'description'=>'Da giocatore professionista di basket a sviluppatore web. 6 mesi di impegno da MVP e un memorabile tap-in targato Boolean hanno garantito ad Alessandro un solido futuro come web developer.'
+  ]
+];
