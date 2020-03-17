bc1291ea93170868e1200c0ee2e580fd3c91b1b2
Matteo <matteop18@gmail.com>
2020-03-17 21:44:46 +0100

    fine

--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
-    <title>Document</title>
+    <link rel="icon" href="https://www.boolean.careers/images/favicon/favicon.ico" />
+    <title>Boolean</title>
 </head>
 <body>
     <div class="wrapper">
