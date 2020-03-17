1578b892c9a0bb2e72be69c9f55f61395c91716b
Matteo <matteop18@gmail.com>
2020-03-17 18:48:08 +0100

    inizio css card student

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -2,11 +2,15 @@
 @extends('layouts.layout')
 @section('main')
 {{-- Posso usare config anche per ciclarci dentro --}}
+<div class="container">
     @foreach (config('students.allStudents') as $item)
+    <div class="card-student">
         <img src="{{$item['poster']}}" alt="">
         <h2>{{$item['name']}} ({{$item['age']}})</h2>
         <p>Lavora da {{$item['company']}} come {{$item['role']}}</p>
         <p>{{$item['description']}}</p>
+    </div>
     @endforeach
+</div>
 @endsection
 
