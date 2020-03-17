eb82847af69debd50003887f1745127706fceac2
Matteo <matteop18@gmail.com>
2020-03-17 18:39:26 +0100

    età

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -4,7 +4,7 @@
 {{-- Posso usare config anche per ciclarci dentro --}}
     @foreach (config('students.allStudents') as $item)
         <img src="{{$item['poster']}}" alt="">
-        {{$item['name']}}
+        {{$item['name']}} ({{$item['age']}})
     @endforeach
 @endsection
 
