b87af210303b5e7bdcd389246bb31f81a3b0df1a
Matteo <matteop18@gmail.com>
2020-03-20 15:39:15 +0100

    model

--- app/Student.php
+++ app/Student.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    // Inserisco tutti i campi che ho come colonne nel database che posso modificare
+
+    protected $fillable = [
+        'poster',
+        'name',
+        'age',
+        'company',
+        'role',
+        'description'
+    ];
 }
