f98f42a5eb88b142d7c7203bef5e6a356ddfcbaa
Matteo <matteop18@gmail.com>
2020-03-22 15:21:49 +0100

    Footer

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,3 +1,46 @@
+<div class="container-footer">
+    <div class="box-footer">
+        <div class="footer-left">
+            <ul>
+                <li>
+                    <img src="https://www.boolean.careers/images/common/logo-white.png" alt="">
+                </li>
+                <li>Sede Operativa</li>
+                <li>Via Carducci 12 - 20123 Milano</li>
+                <li>Tel: 02-40031288</li>
+            </ul>
+        </div>
+        <div class="footer-right">
+            <ul>
+                <li>Lezione gratuita</li>
+                <li>Domande frequenti</li>
+                <li>Per le aziende</li>
+                <li>Privacy policy</li>
+                <li>Lavora con noi</li>
+            </ul>
+        </div>
+    </div>
+    <div class="container-social">
+        <ul>
+            <li>
+                <i class="fab fa-instagram"></i>
+            </li>
+            <li>
+                <i class="fab fa-facebook"></i>
+            </li>
+            <li>
+                <i class="fab fa-linkedin"></i>
+            </li>
+            <li>
+                <i class="fab fa-youtube"></i>
+            </li>
+        </ul>
+    </div>
+    <div class="box-info">
+        <p>Boolean SRL - Piazzale Giovanni dalle Bande Nere, 9 - 20146, Milano - P.IVA: 10214930967
+        </p>
+    </div>
+</div>
     </div>
 </body>
 </html>
